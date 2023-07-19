13d3cf37f7e67697bc8d351ff97db3bcaaa0cf78
mosesmulumba <mulumbamoses94@gmail.com>
2023-07-19 15:13:43 +0300

    fetched the movie data

---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return ( 
+        <div className="header">
+            <h1>mhotify.com</h1>
+            <h3>We present to you the latest movies and series</h3>
+        </div>
+     );
+}
+
+export default Header;
---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+// import Movie from "./movie";
+
+// function Home({}) {
+
+//     // const data = response.data.results;
+//     return ( 
+//         <>
+           
+//         </>
+
+//      );
+// }
+
+// export default Home;
